4223c5c4da8eb9588d145a669ccf68ffe5c3f0d3
luojbin <z1_2x@msn.com>
2019-12-12 19:02:30 +0800

    简单队列

--- origin/src/main/java/com/loyofo/rabbitmq/origin/consumer/C1_SimpleQueue.java
+++ origin/src/main/java/com/loyofo/rabbitmq/origin/consumer/C1_SimpleQueue.java
@@ -4,9 +4,7 @@ import com.loyofo.rabbitmq.constant.RabbitConstant;
 import com.loyofo.rabbitmq.util.RabbitUtil;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.DeliverCallback;
-
-import java.nio.charset.StandardCharsets;
+import com.rabbitmq.client.GetResponse;
 
 public class C1_SimpleQueue {
     public static void main(String[] argv) throws Exception {
@@ -18,12 +16,23 @@ public class C1_SimpleQueue {
         // 声明队列
         channel.queueDeclare(RabbitConstant.SIMPLE_QUEUE, false, false, false, null);
 
-
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            System.out.println(" [x] Received '" + message + "'");
-        };
-        channel.basicConsume(RabbitConstant.SIMPLE_QUEUE, true, deliverCallback, consumerTag -> {});
-
+        // 拉取消息
+        GetResponse response = channel.basicGet(RabbitConstant.SIMPLE_QUEUE, false);
+        if (response != null) {
+            String data = new String(response.getBody());
+            System.out.println("Get message <= " + data);
+            channel.basicAck(response.getEnvelope().getDeliveryTag(), false);
+        } else {
+            System.out.println("简单队列暂时没有消息");
+        }
+        //关闭通道和连接
+        channel.close();
+        connection.close();
+        // 监听模式
+        // DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+        //     String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+        //     System.out.println(" [x] Received '" + message + "'");
+        // };
+        // channel.basicConsume(RabbitConstant.SIMPLE_QUEUE, true, deliverCallback, consumerTag -> {});
     }
 }
