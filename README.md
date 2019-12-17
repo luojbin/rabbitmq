87d9a309085eeda597cdb3e6cdcd2c5b0c0cfd05
luojbin <z1_2x@msn.com>
2019-12-17 16:42:23 +0800

    修改服务器ip

--- README.md
+++ README.md
@@ -12,3 +12,10 @@
         - **Rabbit-origin** 测试环境变量-原生客户端Demo
         - **Rabbit-spring** 测试环境变量-与Spring集成Demo
         - **Rabbit-springBoot** 测试环境变量-与SpringBoot集成Demo
+
+注: 本 demo 使用的服务器需要在系统 host 文件内指定具体的 ip
+
+```
+# rabbitmq server ip
+192.168.xxx.xxx my-server-rabbitmq
+```
--- common/src/main/java/com/loyofo/rabbitmq/constant/RabbitConstant.java
+++ common/src/main/java/com/loyofo/rabbitmq/constant/RabbitConstant.java
@@ -2,7 +2,7 @@ package com.loyofo.rabbitmq.constant;
 
 public class RabbitConstant {
 
-    public static final String HOST = "192.168.30.104";
+    public static final String HOST = "my-server-rabbitmq";
     public static final int PORT = 5672;
     public static final String USERNAME = "guest";
     public static final String PASSWORD = "guest";
